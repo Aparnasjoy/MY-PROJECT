2fef34a8491524400d4860ea3ad0d40b6e977a51
Aparnasjoy <117843505+Aparnasjoy@users.noreply.github.com>
2024-03-01 09:00:18 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'screen/home_page.dart'; // Import your AddAccountPage file
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Set this to false to remove the debug banner
+      home: HomePage(),
+    );
+  }
+}
